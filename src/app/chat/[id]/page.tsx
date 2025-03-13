4a42b368de898513bd4ef054f608425043e04f19
tintin <abctintin0504@gmail.com>
2025-03-14 04:49:35 +0800

    fix

--- src/app/chat/[id]/page.tsx
+++ src/app/chat/[id]/page.tsx
@@ -1,5 +1,7 @@
+'use client';
+
 import { Chat } from "./Chat";
 
-export default async function ChatPage({ params }: { params: { id: string } }) {
+export default function ChatPage({ params }: { params: { id: string } }) {
   return <Chat id={params.id} />;
 }
