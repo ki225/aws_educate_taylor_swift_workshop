fc31f8b39379fa977f68cd74fd3e450dc86cd314
tintin <abctintin0504@gmail.com>
2025-03-20 14:49:48 +0800

    chore: image domain

--- next.config.js
+++ next.config.js
@@ -1,15 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // output: 'export',
-   
-    // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-    // trailingSlash: true,
-   
-    // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-    // skipTrailingSlashRedirect: true,
-   
-    // Optional: Change the output directory `out` -> `dist`
-    // distDir: 'dist',
-  }
-   
-  module.exports = nextConfig
+  // output: 'export',
+
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+  images: {
+    domains: [
+      "20250329-aws-educate-taylor-swift-workshop.s3.ap-northeast-1.amazonaws.com",
+      "example.com"
+    ],
+  },
+};
+
+module.exports = nextConfig;
