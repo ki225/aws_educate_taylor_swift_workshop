1bd64d1f7ab3569966db592ea4c81b45e416b68c
tintin <abctintin0504@gmail.com>
2025-03-25 10:55:45 +0800

    feat: lambda invoke mutation

--- amplify/functions/BusinessAnalyzer/graphql.ts
+++ amplify/functions/BusinessAnalyzer/graphql.ts
@@ -1,6 +1,7 @@
 export const publishResult = /* GraphQL */ `
   mutation PublishResult($sessionId: String!, $imageUrl: String!, $description: String!) {
     publishResult(sessionId: $sessionId, imageUrl: $imageUrl, description: $description) {
+      sessionId
       imageUrl
       description
     }
